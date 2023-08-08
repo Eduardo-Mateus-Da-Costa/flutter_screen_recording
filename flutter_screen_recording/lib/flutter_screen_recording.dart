84093eb7b3e7601061e944f600de977a38e0a961
Adnan Atif <m.adnan.atif@piecyfer.com>
2023-08-08 17:48:21 +0500

    fix issues

--- flutter_screen_recording/lib/flutter_screen_recording.dart
+++ flutter_screen_recording/lib/flutter_screen_recording.dart
@@ -31,39 +31,39 @@ class FlutterScreenRecording {
     return path;
   }
 
-  static  _maybeStartFGS(String titleNotification, String messageNotification) async {
-    if (!kIsWeb && Platform.isAndroid) {
-
-      await FlutterForegroundTask.init(
-        androidNotificationOptions: AndroidNotificationOptions(
-          channelId: 'notification_channel_id',
-          channelName: titleNotification,
-          channelDescription: messageNotification,
-          channelImportance: NotificationChannelImportance.LOW,
-          priority: NotificationPriority.LOW,
-          iconData: const NotificationIconData(
-            resType: ResourceType.mipmap,
-            resPrefix: ResourcePrefix.ic,
-            name: 'launcher',
-          ),
-          buttons: [
-            // const NotificationButton(id: 'sendButton', text: 'Send'),
-            // const NotificationButton(id: 'testButton', text: 'Test'),
-          ],
-        ),
-        // iosNotificationOptions: const IOSNotificationOptions(
-        //   showNotification: true,
-        //   playSound: false,
-        // ),
-        // foregroundTaskOptions: const ForegroundTaskOptions(
-        //   interval: 5000,
-        //   autoRunOnBoot: true,
-        //   allowWifiLock: true,
-        // ),
-        // printDevLog: true,
-      );
-    }
-  }
+  // static  _maybeStartFGS(String titleNotification, String messageNotification) async {
+  //   if (!kIsWeb && Platform.isAndroid) {
+  //
+  //     await FlutterForegroundTask.init(
+  //       androidNotificationOptions: AndroidNotificationOptions(
+  //         channelId: 'notification_channel_id',
+  //         channelName: titleNotification,
+  //         channelDescription: messageNotification,
+  //         channelImportance: NotificationChannelImportance.LOW,
+  //         priority: NotificationPriority.LOW,
+  //         iconData: const NotificationIconData(
+  //           resType: ResourceType.mipmap,
+  //           resPrefix: ResourcePrefix.ic,
+  //           name: 'launcher',
+  //         ),
+  //         buttons: [
+  //           // const NotificationButton(id: 'sendButton', text: 'Send'),
+  //           // const NotificationButton(id: 'testButton', text: 'Test'),
+  //         ],
+  //       ),
+  //       iosNotificationOptions: const IOSNotificationOptions(
+  //         showNotification: true,
+  //         playSound: false,
+  //       ),
+  //       // foregroundTaskOptions: const ForegroundTaskOptions(
+  //       //   interval: 5000,
+  //       //   autoRunOnBoot: true,
+  //       //   allowWifiLock: true,
+  //       // ),
+  //       // printDevLog: true,
+  //     );
+  //   }
+  // }
 
   static void globalForegroundService() {
     print("current datetime is ${DateTime.now()}");
