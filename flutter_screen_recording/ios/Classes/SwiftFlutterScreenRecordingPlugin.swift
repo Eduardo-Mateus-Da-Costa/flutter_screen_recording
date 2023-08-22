5787cde140939a62a83c2a4aee08227d22705602
Adnan Atif <m.adnan.atif@piecyfer.com>
2023-08-22 12:40:36 +0500

    video call issue

--- flutter_screen_recording/ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
+++ flutter_screen_recording/ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
@@ -93,11 +93,20 @@ let screenSize = UIScreen.main.bounds
                         
             if(recordAudio){
                 
-                let audioOutputSettings: [String : Any] = [
-                    AVNumberOfChannelsKey : 2,
-                    AVFormatIDKey : kAudioFormatMPEG4AAC,
-                    AVSampleRateKey: 44100,
-                ]
+//                 let audioOutputSettings: [String : Any] = [
+//                     AVNumberOfChannelsKey : 6,
+//                     AVFormatIDKey : kAudioFormatMPEG4AAC,
+//                     AVSampleRateKey: 44100,
+//                 ]
+
+                 var channelLayout = AudioChannelLayout.init()
+                            channelLayout.mChannelLayoutTag = kAudioChannelLayoutTag_MPEG_5_1_D
+                            let audioOutputSettings: [String : Any] = [
+                                AVNumberOfChannelsKey: 6,
+                                AVFormatIDKey: kAudioFormatMPEG4AAC_HE,
+                                AVSampleRateKey: 44100,
+                                AVChannelLayoutKey: NSData(bytes: &channelLayout, length: MemoryLayout.size(ofValue: channelLayout)),
+                                ]
                 
                 audioInput = AVAssetWriterInput(mediaType: AVMediaType.audio, outputSettings: audioOutputSettings)
                 videoWriter?.add(audioInput)
