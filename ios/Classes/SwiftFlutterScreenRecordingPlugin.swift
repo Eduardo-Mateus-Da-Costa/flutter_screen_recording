30235b745b70b7c26234b161988422f440055ea2
Eduardo Mateus Da Costa <dudu.costatx11@gmail.com>
2024-04-04 13:23:32 -0300

    Swift novo

--- ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
+++ ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
@@ -1,9 +1,43 @@
 import Flutter
-import Foundation
-import AVFoundation
+import UIKit
+import ReplayKit
+import Photos
+
+struct RecorderConfig {
+    var fileName: String = ""
+    var dirPathToSave:NSString = ""
+    var isAudioEnabled: Bool = false
+    var addTimeCode: Bool! = false
+    var filePath: NSString = ""
+    var videoFrame: Int?
+    var videoBitrate: Int? = 5000
+    var fileOutputFormat: String = ""
+    var fileExtension: String = ""
+    var videoHash: String = ""
+    var width:Int?
+    var height:Int?
+}
 
 public class SwiftFlutterScreenRecordingPlugin: NSObject, FlutterPlugin {
-    var screenRecorder: ScreenRecorder?
+
+    let recorder = RPScreenRecorder.shared()
+    var videoOutputURL : URL?
+    var videoWriter : AVAssetWriter?
+    var audioInput:AVAssetWriterInput!
+    var internalAudioInput:AVAssetWriterInput!
+    var videoWriterInput : AVAssetWriterInput?
+
+    var success: Bool = false
+    var startDate: Int?
+    var endDate: Int?
+    var isProgress: Bool = false
+    var eventName: String = ""
+    var message: String = ""
+
+
+    var myResult: FlutterResult?
+
+    var recorderConfig:RecorderConfig = RecorderConfig()
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_screen_recording", binaryMessenger: registrar.messenger())
@@ -12,88 +46,202 @@ public class SwiftFlutterScreenRecordingPlugin: NSObject, FlutterPlugin {
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-       if call.method == "startScreenRecording" {
-        do {
-            if let args = call.arguments as? [String: Any], let filePath = args["filePath"] as? String {
-                let destinationUrl = URL(fileURLWithPath: filePath)
-                try screenRecorder = ScreenRecorder(destination: destinationUrl)
-                try screenRecorder?.start()
-                result(true)
-            } else {
-                result(FlutterError(code: "INVALID_ARGUMENT", message: "Missing filePath argument", details: nil))
+
+        if(call.method == "startRecordScreen"){
+            let args = call.arguments as? Dictionary<String, Any>
+            recorderConfig = RecorderConfig()
+            let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString
+            recorderConfig.isAudioEnabled=((args?["audio"] as? Bool?)! ?? (args?["internalaudio"] as? Bool?)! ?? false)!
+            recorderConfig.fileName=(args?["name"] as? String)!
+            recorderConfig.dirPathToSave = documentsPath
+            recorderConfig.addTimeCode=true
+            recorderConfig.videoFrame=30
+
+            if UIDevice.current.orientation.isLandscape {
+                if(recorderConfig.width == nil) {
+                    recorderConfig.width = Int(UIScreen.main.nativeBounds.height)
+                }
+                if(recorderConfig.height == nil) {
+                    recorderConfig.height = Int(UIScreen.main.nativeBounds.width)
+                }
+            }else{
+                if(recorderConfig.width == nil) {
+                    recorderConfig.width = Int(UIScreen.main.nativeBounds.width)
+                }
+                if(recorderConfig.height == nil) {
+                    recorderConfig.height = Int(UIScreen.main.nativeBounds.height)
+                }
             }
-        } catch {
-            result(false)
+            self.success=Bool(startRecording(width: Int32(recorderConfig.width!) ,height: Int32(recorderConfig.height!)))
+
+            myResult = result
+            result(self.success)
+        }else if(call.method == "stopRecordScreen"){
+            if(videoWriter != nil){
+                self.success=Bool(stopRecording())
+            }else{
+                self.success=Bool(false)
+            }
+            myResult = result
+            if (self.success){
+                NSLog("Success")
+                NSLog(recorderConfig.filePath as String)
+                NSLog(recorderConfig.fileName as String)
+                result(recorderConfig.filePath.appendingPathComponent(recorderConfig.fileName) as String)
+                // var fileName: String = String("\(recorderConfig.filePath)/\(recorderConfig.fileName)"),
+                result(recorderConfig.filePath.appendingPathComponent(recorderConfig.fileName))
+            }
+            else {
+                result("")
+            }
         }
-    } else if call.method == "stopScreenRecording" {
-        screenRecorder?.stop()
-        result(nil)
-    } else {
-        result(FlutterMethodNotImplemented)
-    }
-}
-
-    func captureFullScreen() -> Data? {
-        UIGraphicsBeginImageContextWithOptions(UIScreen.main.bounds.size, false, 0.0)
-        defer { UIGraphicsEndImageContext() }
-        guard let context = UIGraphicsGetCurrentContext() else { return nil }
-        for window in UIApplication.shared.windows {
-            window.layer.render(in: context)
-        }
-        return UIGraphicsGetImageFromCurrentImageContext()?.pngData()
-    }
-}
-
-
-open class ScreenRecorder: NSObject, AVCaptureFileOutputRecordingDelegate {
-    let destinationUrl: URL
-    let session: AVCaptureSession
-    let movieFileOutput: AVCaptureMovieFileOutput
-
-    open var destination: URL {
-        get {
-            return self.destinationUrl
+        else {
+            result("")
         }
     }
 
-    public init(destination: URL) {
-        destinationUrl = destination
+    func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
 
-        session = AVCaptureSession()
-        session.sessionPreset = AVCaptureSessionPresetHigh
+    @objc func startRecording(width: Int32, height: Int32) -> Bool {
+        var res : Bool = true
+        if(recorder.isAvailable){
+            if recorderConfig.dirPathToSave != "" {
+                recorderConfig.filePath = (recorderConfig.dirPathToSave ) as NSString
+                self.videoOutputURL = URL(fileURLWithPath: String(recorderConfig.filePath.appendingPathComponent(recorderConfig.fileName ) ))
+            } else {
+                recorderConfig.filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString
+                self.videoOutputURL = URL(fileURLWithPath: String(recorderConfig.filePath.appendingPathComponent(recorderConfig.fileName ) ))
+            }
+            do {
+                let fileManager = FileManager.default
+                if (fileManager.fileExists(atPath: videoOutputURL!.path)){
+                    try FileManager.default.removeItem(at: videoOutputURL!)}
+            } catch let fileError as NSError{
+                self.message=String(fileError as! Substring) as String
+                res = Bool(false);
+            }
 
-        let displayId: CGDirectDisplayID = CGDirectDisplayID(CGMainDisplayID())
+            do {
+                try videoWriter = AVAssetWriter(outputURL: videoOutputURL!, fileType: AVFileType.mp4)
+                self.message=String("Started Video")
+            } catch let writerError as NSError {
+                self.message=String(writerError as! Substring) as String
+                videoWriter = nil
+                res = Bool(false)
+            }
+            if #available(iOS 11.0, *) {
+                recorder.isMicrophoneEnabled = recorderConfig.isAudioEnabled
+                let videoSettings: [String : Any] = [
+                    AVVideoCodecKey  : AVVideoCodecType.h264,
+                    AVVideoWidthKey  : NSNumber.init(value: width),
+                    AVVideoHeightKey : NSNumber.init(value: height),
+                    AVVideoCompressionPropertiesKey: [
+                        AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel,
+                        AVVideoAverageBitRateKey: recorderConfig.videoBitrate!
+                    ] as [String : Any],
+                ]
+                self.videoWriterInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: videoSettings)
+                self.videoWriterInput?.expectsMediaDataInRealTime = true
+                self.videoWriter?.add(videoWriterInput!);
+                if(recorderConfig.isAudioEnabled) {
+                    let audioOutputSettings: [String : Any] = [
+                        AVNumberOfChannelsKey : 2,
+                        AVFormatIDKey : kAudioFormatMPEG4AAC,
+                        AVSampleRateKey: 44100,
+                        AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                    ]
+                    self.audioInput = AVAssetWriterInput(mediaType: AVMediaType.audio, outputSettings: audioOutputSettings)
+                    self.audioInput?.expectsMediaDataInRealTime = true
+                    self.videoWriter?.add(audioInput!)
 
-        let input: AVCaptureScreenInput = AVCaptureScreenInput(displayID: displayId)
+                    self.internalAudioInput = AVAssetWriterInput(mediaType: AVMediaType.audio, outputSettings: audioOutputSettings)
+                    self.internalAudioInput?.expectsMediaDataInRealTime = true
+                    self.videoWriter?.add(internalAudioInput!)
+                }
 
-
-        if session.canAddInput(input) {
-            session.addInput(input)
+                recorder.startCapture(
+                    handler: {
+                    (cmSampleBuffer, rpSampleType, error) in guard error == nil else { return }
+                        switch rpSampleType {
+                            case RPSampleBufferType.video:
+                                if self.videoWriter?.status == AVAssetWriter.Status.unknown {
+                                    self.videoWriter?.startWriting()
+                                    self.videoWriter?.startSession(atSourceTime:  CMSampleBufferGetPresentationTimeStamp(cmSampleBuffer));
+                                }else if self.videoWriter?.status == AVAssetWriter.Status.writing {
+                                    if (self.videoWriterInput?.isReadyForMoreMediaData == true) {
+                                        if  self.videoWriterInput?.append(cmSampleBuffer) == false {
+                                            res = Bool(false)
+                                            self.message="Error starting capture"
+                                        }
+                                    }
+                                }
+                            case RPSampleBufferType.audioMic:
+                                if (self.recorderConfig.isAudioEnabled){
+                                    if self.audioInput?.isReadyForMoreMediaData == true {
+                                        if self.audioInput?.append(cmSampleBuffer) == false {
+                                            NSLog("Audio mic writing error")
+                                            NSLog("\(self.videoWriter?.status)")
+                                            NSLog("\(self.videoWriter?.error)")
+                                        }
+                                    }
+                                }
+                            case RPSampleBufferType.audioApp:
+                               if (self.recorderConfig.isAudioEnabled){
+                                    if self.internalAudioInput?.isReadyForMoreMediaData == true {
+                                        if self.internalAudioInput?.append(cmSampleBuffer) == false {
+                                            NSLog("Audio app writing error")
+                                            NSLog("\(self.videoWriter?.status)")
+                                            NSLog("\(self.videoWriter?.error)")
+                                        }
+                                    }
+                                }
+                        default:
+                            break;
+                        }
+                    }){(error) in guard error == nil else {
+                        return
+                    }
+                }
+            }
         }
+        return  Bool(res)
+    }
 
-        movieFileOutput = AVCaptureMovieFileOutput()
+    @objc func stopRecording() -> Bool {
+        var res : Bool = true;
+        if(recorder.isRecording){
+            if #available(iOS 11.0, *) {
 
-        if session.canAddOutput(movieFileOutput) {
-            session.addOutput(movieFileOutput)
+                recorder.stopCapture( handler: { (error) in
+                    if(error != nil){
+                        res = Bool(false)
+                        self.message = "Has Got Error in stop record"
+                    }
+                })
+            } else {
+                res = Bool(false)
+                self.message="You dont Support this plugin"
+            }
+
+            self.videoWriterInput?.markAsFinished();
+            if(recorderConfig.isAudioEnabled) {
+                self.audioInput?.markAsFinished();
+                self.internalAudioInput?.markAsFinished();
+            }
+
+            self.videoWriter?.finishWriting {
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: self.videoOutputURL!)
+                })
+                self.message="stopRecordScreenFromApp"
+            }
+        } else{
+            self.message="You haven't start the recording unit now!"
         }
+        return Bool(res)
 
-    }
-
-    open func start() {
-        session.startRunning()
-        movieFileOutput.startRecording(toOutputFileURL: self.destinationUrl, recordingDelegate: self)
-    }
-
-    open func stop() {
-        movieFileOutput.stopRecording()
-    }
-
-    open func capture(
-        _ captureOutput: AVCaptureFileOutput!,
-        didFinishRecordingToOutputFileAt outputFileURL: URL!,
-        fromConnections connections: [Any]!,
-        error: Error!
-    ) {
-        session.stopRunning()
     }
 }
