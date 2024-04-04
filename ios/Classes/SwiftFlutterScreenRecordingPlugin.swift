f3a66d70dfa9386ffe6525682d87b7a09e202604
Eduardo Mateus Da Costa <dudu.costatx11@gmail.com>
2024-04-04 12:10:30 -0300

    As String

--- ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
+++ ios/Classes/SwiftFlutterScreenRecordingPlugin.swift
@@ -178,7 +178,7 @@ public class SwiftFlutterScreenRecordingPlugin: NSObject, FlutterPlugin {
                                     if self.audioInput?.isReadyForMoreMediaData == true {
                                         if self.audioInput?.append(cmSampleBuffer) == false {
                                             NSLog("Audio mic writing error" as String)
-                                            NSLog(self.videoWriter?.status ?? "")
+                                            NSLog(self.videoWriter?.status as String ?? "")
                                             NSLog(self.videoWriter?.error ?? "")
                                         }
                                     }
@@ -188,7 +188,7 @@ public class SwiftFlutterScreenRecordingPlugin: NSObject, FlutterPlugin {
                                     if self.audioInput?.isReadyForMoreMediaData == true {
                                         if self.audioInput?.append(cmSampleBuffer) == false {
                                             NSLog("Audio app writing error")
-                                            NSLog(self.videoWriter?.status ?? "")
+                                            NSLog(self.videoWriter?.status as String ?? "")
                                             NSLog(self.videoWriter?.error ?? "")
                                         }
                                     }
