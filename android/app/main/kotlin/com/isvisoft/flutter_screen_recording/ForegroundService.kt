d41ab07c8d1bb73dbab941c2d229fe277cd1712d
Eduardo Mateus Da Costa <dudu.costatx11@gmail.com>
2024-05-15 14:04:07 -0300

    Tentando um novo jeito de iniciar a notificação

--- android/app/main/kotlin/com/isvisoft/flutter_screen_recording/ForegroundService.kt
+++ android/app/main/kotlin/com/isvisoft/flutter_screen_recording/ForegroundService.kt
@@ -1,4 +1,4 @@
-package android.app.main.kotlin.com.isvisoft.flutter_screen_recording
+package com.isvisoft.flutter_screen_recording
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -14,56 +14,27 @@ import com.isvisoft.flutter_screen_recording.R
 
 
 class ForegroundService : Service() {
-    private val CHANNEL_ID = "ForegroundService Kotlin"
-    companion object {
-        fun startService(context: Context, title: String, message: String) {
-            val startIntent = Intent(context, ForegroundService::class.java)
-            startIntent.putExtra("messageExtra", message)
-            startIntent.putExtra("titleExtra", title)
-            ContextCompat.startForegroundService(context, startIntent)
-        }
-        fun stopService(context: Context) {
-            val stopIntent = Intent(context, ForegroundService::class.java)
-            context.stopService(stopIntent)
-        }
-    }
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    private const val NOTIFICATION_CHANNEL_ID = "general_notification_channel"
 
-        var title = intent?.getStringExtra("titleExtra")
-        if (title == null) {
-            title = "Flutter Screen Recording";
-        }
-        var message = intent?.getStringExtra("messageExtra")
-        if (message == null) {
-            message = ""
-        }
-
-        createNotificationChannel()
-        val notificationIntent = Intent(this, FlutterScreenRecordingPlugin::class.java)
-
-        val pendingIntent = PendingIntent.getActivity(
-                this,
-                0, notificationIntent, PendingIntent.FLAG_MUTABLE
-        )
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setSmallIcon(R.drawable.icon)
-                .setContentIntent(pendingIntent)
-                .build()
-        startForeground(1, notification)
-
-        return START_NOT_STICKY
-    }
-    override fun onBind(intent: Intent): IBinder? {
-        return null
-    }
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val serviceChannel = NotificationChannel(CHANNEL_ID, "Foreground Service Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT)
-            val manager = getSystemService(NotificationManager::class.java)
-            manager!!.createNotificationChannel(serviceChannel)
+    private fun startService(context: Context, title: String, content: String) {
+        Notification.Builder(this, NOTIFICATION_CHANNEL_ID).apply {
+            setContentTitle(title)
+            setOngoing(true)
+            setContentText(content)
+            setSmallIcon(R.drawable.ic_notifcation_icon)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            NotificationChannel(NOTIFICATION_CHANNEL_ID, "General", importance).apply {
+                description = "General Notifications"
+                with((this@LocationService.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)) {
+                    createNotificationChannel(this@apply)
+                }
+            }
+            //need core 1.12 and higher and SDK 29 and higher
+            ServiceCompat.startForeground(
+                this@LocationService, 1, this.build(),
+                ServiceInfo.MEDIA_PROJECTION_SERVICE
+            )
+            //this@LocationService.startForeground(1, this.build())
         }
     }
 }
